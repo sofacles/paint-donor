c78a32ab68c0eb2dff98185a0240fdc1a6702fb0
Steve Degermark <=>
2019-10-21 17:16:56 -0700

    adding two cypress tests

--- README.md
+++ README.md
@@ -6,14 +6,19 @@ An app for getting rid of your scrap paint.  I plan to add an email redirection 
 ## Prerequisites
 1. Download https://www.mongodb.com/download-center/community 
 2. Clone the repo
-3. cd into it and run yarn
-4. cd into the server folder and run yarn
+3. `cd` into it and run yarn
+4. `cd` into the server folder and run yarn
 
 
 
 ## Tests
-1. client-side tests: run `yarn test`
-2. server-side tests cd server, and run `yarn test`
+- Client-side tests: run `yarn test`
+- Server-side tests cd server, and run `yarn test`
+- E2E tests
+    1. In the root run the app locally
+    2. In a separate shell run `./node_modules/.bin/cypress open`
+    3. Click a link under "integration tests"
 
 ## Run the app locally
-1. cd to the root of the application and run `yarn dev`
+1. `cd` to the root of the application and run `yarn dev`
+2. Click "give away paint"  (don't miss the color picker, click the icon next to "choose color)
---
+++ cypress/integration/requiredFieldsTest.js
@@ -0,0 +1,31 @@
+describe("navigates..", () => {
+
+    it('Posts and goes to thank you if all fields are valid', function() {
+        cy.fixture('paint/happyPath').then((json) => {
+            cy.visit('http://localhost:3000/giveawaypaint')
+            cy.get("#brand").type(json.brand);
+            cy.get("#name").type(json.name);
+            cy.get("#quantity").type(json.quantity);
+            cy.get("#email").type(json.email);
+            cy.get("#confirmEmail").type(json.confirmEmail);
+            cy.get("#rgbDisplay").type(json.rgb);
+            cy.get("#save").click();
+            cy.url().should('include', '/ThankYou')
+            cy.get("div").should('contain', 'Thanks for posting your paint');
+        })
+    })
+
+    it('Informs you that emails need to match', function() {
+        cy.fixture('paint/happyPath').then((json) => {
+            cy.visit('http://localhost:3000/giveawaypaint')
+            cy.get("#brand").type(json.brand);
+            cy.get("#name").type(json.name);
+            cy.get("#quantity").type(json.quantity);
+            cy.get("#email").type(json.email);
+            cy.get("#confirmEmail").type("notTheSame");
+            cy.get("#rgbDisplay").click();
+           
+            cy.get("span[data-testid='confirm-email-error']").should('contain', 'match');
+        })
+    })
+});
