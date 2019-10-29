ae5929b0b8f4e3b0a7ec9d700a29a723775992b5
Steve Degermark <=>
2019-10-29 14:03:53 -0700

    rgb in background behind img

--- src/components/PaintTile.jsx
+++ src/components/PaintTile.jsx
@@ -3,18 +3,28 @@ export const PaintTile = ({paintUnit}) => {
     const needHash = paintUnit.rgb && paintUnit.rgb[0] !== '#';
     const rgbStyle = {
         height: "50px",
-        backgroundColor: `${needHash ? '#': ''}${paintUnit.rgb && paintUnit.rgb.length > 0 ? paintUnit.rgb : "#fff"}`
+        backgroundColor: `${needHash ? '#': ''}${paintUnit.rgb && paintUnit.rgb.length > 0 
+            ? paintUnit.rgb : "#fff"}`,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
     };
-    const image = (paintUnit.imageName && paintUnit.imageName.length > 3) ? 
-    <div>
-        <img alt="paint color" style={rgbStyle} src={`uploads/${paintUnit.imageName}`} />
-    </div> : <div style={rgbStyle} ></div>;
+    const imgStyle = {
+        height: "90%",
+        margin: "5px"
+    };
+    const image = (paintUnit.imageName && paintUnit.imageName.length > 3)
+    ? <img alt="paint color" style={imgStyle} src={`uploads/${paintUnit.imageName}`} /> 
+    : "";
 
     return <div className="paint-cell">
     <span>{paintUnit.brand}</span>
     <span>{paintUnit.name}</span>
     <span>{paintUnit.quantity}</span>
+    <div style={rgbStyle} >
     {image}
+    </div>
+   
 
     </div>
 };
