3d6c59342c36965f03ea7a27b8e55915576e3010
Steve Degermark <=>
2019-10-29 11:05:30 -0700

    beginnings of tiles, static image path borken

--- src/components/PaintTile.jsx
+++ src/components/PaintTile.jsx
@@ -1,4 +1,20 @@
 import React from "react";
-export const PaintTile = (props) => {
-    return <div>{props.paintUnit.name}</div>
+export const PaintTile = ({paintUnit}) => {
+    const needHash = paintUnit.rgb && paintUnit.rgb[0] != '#';
+    const rgbStyle = {
+        height: "50px",
+        backgroundColor: `${needHash ? '#': ''}${paintUnit.rgb && paintUnit.rgb.length > 0 ? paintUnit.rgb : "#fff"}`
+    };
+    const image = (paintUnit.imageName && paintUnit.imageName.length > 3) ? 
+    <div>
+        <img alt="paint color" src={`/uploads/${paintUnit.imageName}`} />
+    </div> : <div style={rgbStyle} ></div>;
+
+    return <div className="paint-cell">
+    <span>{paintUnit.brand}</span>
+    <span>{paintUnit.name}</span>
+    <span>{paintUnit.quantity}</span>
+    {image}
+
+    </div>
 };
