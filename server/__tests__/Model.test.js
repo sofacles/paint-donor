2de08c1498497f1683b7461aa511c34ef2cb212d
steve <sofacles@yahoo.com>
2019-11-14 08:56:13 -0800

    a few more mongoose validation tests

--- server/__tests__/Model.test.js
+++ server/__tests__/Model.test.js
@@ -15,7 +15,7 @@ describe("Need to have either RGB or image name", () => {
     }
   });
 
-  test("neither RGB nor imageFile causes an error", async (done) => {
+  test("Empty values for both RGB and imageName causes an error", async (done) => {
     let pChip = new PaintCan({
       brand: "Miller",
       zipCode: "11777",
@@ -29,6 +29,48 @@ describe("Need to have either RGB or image name", () => {
     try {
       result = await pChip.validate(function(error) {
         expect(error).not.toBe(null);
+        done();
+      });
+    } catch (err) {
+      expect(err).not.toBe(null);
+    }
+  });
+
+  test("Valid RGB and and blank imageName is valid", async (done) => {
+    let pChip = new PaintCan({
+      brand: "Miller",
+      zipCode: "11777",
+      name: "avocado",
+      rgb: "333",
+      imageName: "",
+      quantity: "1 gal",
+      email: "beyonce@gmail.com"
+    });
+    let result = "";
+    try {
+      result = await pChip.validate(function(error) {
+        expect(error).toBe(null);
+        done();
+      });
+    } catch (err) {
+      expect(err).not.toBe(null);
+    }
+  });
+
+  test("Valid imageName and and blank RGB is valid", async (done) => {
+    let pChip = new PaintCan({
+      brand: "Miller",
+      zipCode: "11777",
+      name: "avocado",
+      rgb: "",
+      imageName: "898736.png",
+      quantity: "1 gal",
+      email: "beyonce@gmail.com"
+    });
+    let result = "";
+    try {
+      result = await pChip.validate(function(error) {
+        expect(error).toBe(null);
         done();
       });
     } catch (err) {
