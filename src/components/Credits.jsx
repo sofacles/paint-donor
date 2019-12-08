f667258598fdf7dd431b0352b0d1390b79ab8c67
steve <sofacles@yahoo.com>
2019-12-08 15:08:22 -0800

    gitignore config file

---
+++ src/components/Credits.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Credits = () => {
+    <div>
+        <ul>
+            <li><a href="https://rmolinamir.github.io/typescript-cheatsheet/">RMolinamir's Typescript Cheat Sheet </a> helped me figure out useContext in Typescript</li>
+            https://docs.mongodb.com/manual/tutorial/enable-authentication/
+            https://studio3t.com/knowledge-base/articles/create-mongodb-user/
+            https://studio3t.com/knowledge-base/articles/mongodb-users-roles-explained-part-1/
+
+        </ul>
+    </div>
+}
