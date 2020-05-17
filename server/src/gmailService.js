f68244f778f102fb570325a846624c5d486a080f
steve <sofacles@yahoo.com>
2020-05-17 14:09:35 -0700

    second time sending mail

---
+++ server/src/gmailService.js
@@ -0,0 +1,46 @@
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const config = require("../config");
+
+function sendGMail(brand, name) {
+  const oauth2Client = new OAuth2(
+    config.gmail.clientId,
+    config.gmail.clientSecret,
+    "https://developers.google.com/oauthplayground" // Redirect URL
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: config.gmail.refreshToken 
+  });
+  const accessToken = oauth2Client.getAccessToken();
+  const smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+         type: "OAuth2",
+         user: config.gmail.user, 
+         clientId: config.gmail.clientId,
+         clientSecret: config.gmail.clientSecret,
+         refreshToken: config.gmail.refreshToken,
+         accessToken: accessToken
+    }
+  });
+
+
+  const mailOptions = {
+    from: config.gmail.user,
+    to: "somebody.sparkly@gmail.com",
+    subject: "Somebody is interested in your paint",
+    generateTextFromHTML: true,
+    html: `<b>Somebody would like to adopt your ${brand}:${name} paint! </b><div>Do not touch the subject line.  That's how we track which paint is which.</div><img alt="header" src="https://1lesscan.us/apple-icon-144x144.png" />`
+  };
+
+  smtpTransport.sendMail(mailOptions, (error, response) => {
+    error ? console.log(error) : console.log(response);
+    smtpTransport.close();
+  });
+}
+
+
+module.exports = { sendGMail }
--- server/src/routes/sendMail.js
+++ server/src/routes/sendMail.js
@@ -1,11 +1,11 @@
 const { PaintCan } = require("../../models");
 const { decrypt } = require("../../src/cryptoService");
 const { Logger } = require("../logger");
-const runSample = require("./gmailTest")
+const { sendGMail } = require("../gmailService")
 
 const sendMail = async (req, res) => {
     Logger.info("Top of sendMail");
-    await runSample();
+   
     const paintObj = req.body.paint;
 
     const thisPaint = await PaintCan.findOne({_id: paintObj._id});
@@ -15,9 +15,11 @@ const sendMail = async (req, res) => {
         res.send("no such paint");
         return;
     }
+    sendGMail(paintObj.brand, paintObj.name);
     Logger.info(`Posted email: ${thisPaint.email}`);
     const donorEmail = decrypt(thisPaint.email);
     Logger.info(`decrypted email: ${donorEmail}`);
+    Logger.info(JSON.stringify(paintObj));
     try {
         Logger.info("hey Somebody is actually trying to send a mail about a paint!");
         let paintChip = new PaintCan(paintObj);
