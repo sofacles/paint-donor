dda86937fa15813b406412addc1c03fd61d407af
steve <sofacles@yahoo.com>
2020-06-06 19:09:51 -0700

    Giving up on receiving mail.  Can send mail from prod. Moving googleapi dependencies into server, adding logging to gmail service

--- server/src/gmailService.js
+++ server/src/gmailService.js
@@ -3,6 +3,7 @@ const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 
 const config = require("../config");
+const { Logger } = require("./logger");
 
 function sendGMail(brand, name) {
   const oauth2Client = new OAuth2(
@@ -12,35 +13,38 @@ function sendGMail(brand, name) {
   );
 
   oauth2Client.setCredentials({
-    refresh_token: config.gmail.refreshToken 
+    refresh_token: config.gmail.refreshToken,
   });
   const accessToken = oauth2Client.getAccessToken();
   const smtpTransport = nodemailer.createTransport({
     service: "gmail",
     auth: {
-         type: "OAuth2",
-         user: config.gmail.user, 
-         clientId: config.gmail.clientId,
-         clientSecret: config.gmail.clientSecret,
-         refreshToken: config.gmail.refreshToken,
-         accessToken: accessToken
-    }
+      type: "OAuth2",
+      user: config.gmail.user,
+      clientId: config.gmail.clientId,
+      clientSecret: config.gmail.clientSecret,
+      refreshToken: config.gmail.refreshToken,
+      accessToken: accessToken,
+    },
   });
-
 
   const mailOptions = {
     from: config.gmail.user,
     to: "somebody.sparkly@gmail.com",
     subject: "Somebody is interested in your paint",
     generateTextFromHTML: true,
-    html: `<b>Somebody would like to adopt your ${brand}:${name} paint! </b><div>Do not touch the subject line.  That's how we track which paint is which.</div><img alt="header" src="https://1lesscan.us/apple-icon-144x144.png" />`
+    html: `<b>Somebody would like to adopt your ${brand}:${name} paint! </b><div>Do not touch the subject line.  That's how we track which paint is which.</div><img alt="header" src="https://1lesscan.us/apple-icon-144x144.png" />`,
   };
 
-  smtpTransport.sendMail(mailOptions, (error, response) => {
-    error ? console.log(error) : console.log(response);
-    smtpTransport.close();
-  });
+  try {
+    smtpTransport.sendMail(mailOptions, (error, response) => {
+      error ? Logger.error(error) : Logger.info(response);
+      smtpTransport.close();
+    });
+  } catch (err) {
+    Logger.error("error caught in sendGmail!");
+    Logger.error(err);
+  }
 }
 
-
-module.exports = { sendGMail }
+module.exports = { sendGMail };
