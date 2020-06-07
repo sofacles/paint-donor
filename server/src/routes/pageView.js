4637d685b7dea8f5eeb11e77d56431f986bcacc9
steve <sofacles@yahoo.com>
2020-06-06 19:09:51 -0700

    Giving up on receiving mail.  Can send mail from prod. Moving googleapi dependencies into server, adding logging to gmail service

--- server/src/routes/pageView.js
+++ server/src/routes/pageView.js
@@ -1,19 +1,25 @@
 const { Logger } = require("../logger");
-const path = require('path');
+const path = require("path");
 
 const pageView = async (req, res) => {
-  const knownUrls = ['home', 'browse', 'colorPicker', 'donate'];
-  if(!req.query['pg'] || knownUrls.indexOf(req.query['pg']) === -1) {
-    Logger.error(`Somebody is requesting something with a weird qs: ${JSON.stringify(req.query)}`);
+  const knownUrls = ["home", "browse", "colorPicker", "donate"];
+  if (!req.query["pg"] || knownUrls.indexOf(req.query["pg"]) === -1) {
+    Logger.error(
+      `Somebody is requesting something with a weird qs: ${JSON.stringify(
+        req.query
+      )}`
+    );
     res.send("{bar:'gooskis'}");
     return;
-  } 
-  
-  const logMsg = `${req.query['pg']} requested: ${new Date()} from: ${req.connection.remoteAddress}` 
-   + ` Fwd4: ${req.headers['x-forwarded-for']}`;
-   Logger.info(logMsg);
-   res.setHeader('Content-Type','image/png');
-   res.sendFile(path.join(__dirname, '../../images/1x1.png'));
+  }
+
+  const logMsg =
+    `${req.query["pg"]} requested: ${new Date()} from: ${
+      req.connection.remoteAddress
+    }` + ` Fwd4: ${req.headers["x-forwarded-for"]}`;
+  Logger.info(logMsg);
+  res.setHeader("Content-Type", "image/png");
+  res.sendFile(path.join(__dirname, "../../images/1x1.png"));
 };
 
 module.exports = pageView;
