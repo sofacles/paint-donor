d668de1c61ac04bcaa59cc42520f74f94d721318
steve <sofacles@yahoo.com>
2021-03-30 18:53:47 -0700

    turn off errors about explicit return types in js files (explicit-module-boundary-types)

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,17 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
+    overrides: [
+        {
+            // enable the rule specifically for TypeScript files
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                '@typescript-eslint/explicit-module-boundary-types': ['error'],
+            },
+        },
+    ],
     settings: {
         react: {
             version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
