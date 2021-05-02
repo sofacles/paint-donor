d2d7ce027af39cbb038bdc3cec885c68e77cad71
steve <sofacles@yahoo.com>
2021-05-02 16:55:35 -0700

    SendPing spec uses rtl and jest + the Route that handles the redirect url

--- src/components/SendPing.spec.jsx
+++ src/components/SendPing.spec.jsx
@@ -1,19 +1,35 @@
 import SendPing from './SendPing';
 import React from 'react';
-import { waitFor, fireEvent } from '@testing-library/react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
+import { Route, Router } from 'react-router-dom';
 
 import { getByTestId } from '@testing-library/dom';
 import '@testing-library/jest-dom/extend-expect';
 import axios from 'axios';
-import { renderWithRouter } from '../testHelpers/TestHelpers';
+import { createMemoryHistory } from 'history';
 
 jest.mock('axios');
+
+function renderWithRouter(ui, { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {}) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    ...render(
+      <Router history={history}>
+        <Route path="/ThanksForMail">The route you're redirected to</Route>
+        {ui}
+      </Router>,
+    ),
+    history,
+  };
+
+  //hat's off to: https://dev.to/iwazaru/how-to-test-react-router-redirection-with-testing-library-3i36
+}
 
 describe('SendPing', () => {
   beforeEach(() => {
     const resp = {
       data: {
-        status: 200,
+        pingReceived: true,
       },
     };
     axios.post.mockImplementationOnce(() => {
@@ -27,6 +43,7 @@ describe('SendPing', () => {
     fireEvent.click(theButton);
 
     await waitFor(() => expect(axios.post).toHaveBeenCalledTimes(1));
+    await expect(container).toHaveTextContent(/The route you're redirected to/);
 
     expect(axios.post).toHaveBeenCalledWith(`/api/ping/?newThing=1`, { age: 34, favoriteFruit: 'plum' });
   });
