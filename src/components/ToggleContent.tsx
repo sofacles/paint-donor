6ad450bcb5e9180608be87088b25b2a53b2dd9ec
steve <sofacles@yahoo.com>
2021-07-04 19:08:59 -0700

    typing the props to ToggleContent

---
+++ src/components/ToggleContent.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+// hats off to https://wecodetheweb.com/2019/03/02/easy-modals-with-react-hooks/
+
+export type ToggleContentProps = {
+    content: (arg: () => void) => React.ReactElement,
+    toggle: (arg: () => void) => React.ReactElement,
+
+}
+const ToggleContent: React.FC<ToggleContentProps> = ({ toggle, content }: ToggleContentProps) => {
+    const [isShown, setIsShown] = useState(false);
+    const hide = () => setIsShown(false);
+    const show = () => setIsShown(true);
+
+    return (
+        <>
+            {!isShown && toggle(show)}
+            {isShown && content(hide)}
+        </>
+    );
+};
+
+export { ToggleContent };
